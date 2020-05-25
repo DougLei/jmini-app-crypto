c726f95a7e97f0484797d61e4408e0a1f612dfd8
DougLei <953034206@qq.com>
2020-05-25 17:27:43 +0800

    t

--- src/main/java/com/douglei/mini/app/crypto/EncryptorApp.java
+++ src/main/java/com/douglei/mini/app/crypto/EncryptorApp.java
@@ -12,6 +12,8 @@ import com.douglei.tools.utils.StringUtil;
  * @author DougLei
  */
 class EncryptorApp extends CryptoRule{
+	private static final Scanner scanner = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		System.out.println("---------------------------------");
 		System.out.println("欢迎进入加密小程序");
@@ -19,13 +21,11 @@ class EncryptorApp extends CryptoRule{
 		String str;
 		
 		File targetFile = new File(System.getProperty("user.home") + File.separatorChar + "EncryptorApp" + File.separatorChar + "EncryptorApp.txt");
-		Scanner scanner = new Scanner(System.in);
 		Encryptor valueEncryptor = new Encryptor();
-		
 		FileBufferedWriter writer = new FileBufferedWriter(targetFile);
 		try {
 			do {
-				System.out.println("请输入你要加密的字符串, 如果想要结束, 请输入exit(不区分大小写)：");
+				System.out.println("请输入你要加密的字符串， 如果想要结束， 请输入exit（不区分大小写）：");
 				str = scanner.next();
 				if(StringUtil.isEmpty(str)) {
 					System.out.println("请不要输入空字符串");
@@ -36,19 +36,18 @@ class EncryptorApp extends CryptoRule{
 					break;
 				}
 				writer.write(str);
-				writer.write("------");
+				writer.write(" =========加密结果为=========> ");
 				writer.write(valueEncryptor.encrypt(str));
 				writer.newLine();
 			}while(true);
 			writer.close();
-			System.out.println("加密结束, 请去以下文件中获取你的加密数据 ========> " + targetFile);
-			System.out.println("注意: 每行数据------前的为原数据, ------后才是加密后的数据");
-			System.out.println("确认后, 直接关闭该窗口来结束程序");
+			System.out.println("加密结束， 请去以下文件中获取你的加密数据 ========> " + targetFile);
+			System.out.println("确认后， 直接关闭该窗口来结束程序");
 		} catch (IOException e) {
 			e.printStackTrace();
-			System.out.println("你的程序出问题了, 不要关闭该窗口, 并联系开发人员");
+			System.out.println("你的程序出问题了， 不要关闭该窗口， 并联系开发人员");
+			scanner.next();
 		}
 		scanner.next();
-		scanner.close();
 	}
 }
